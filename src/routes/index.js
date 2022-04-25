39ed55046f70d5159534c579faefea7239e9a1d2
yjh4218 <84125522+yjh4218@users.noreply.github.com>
2022-04-25 22:44:19 +0900

    axios_1

---
+++ src/routes/index.js
@@ -0,0 +1,41 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Newsview from "../views/NewsView.vue";
+import Askview from "../views/AskView.vue";
+import Jobsview from "../views/JobsView.vue";
+import ItemView from "../views/ItemView.vue";
+import UserView from "../views/UserView.vue";
+
+Vue.use(VueRouter);
+
+export const router = new VueRouter({
+  mode: "history",
+  routes: [
+    {
+      path: "/",
+      redirect: "/news",
+    },
+    {
+      // path : url 주소
+      path: "/news",
+      //component : url 주소를 갔을 때 표시될 컴포넌트
+      component: Newsview,
+    },
+    {
+      path: "/ask",
+      component: Askview,
+    },
+    {
+      path: "/jobs",
+      component: Jobsview,
+    },
+    {
+      path: "/item",
+      component: ItemView,
+    },
+    {
+      path: "/user",
+      component: UserView,
+    },
+  ],
+});
---
+++ src/views/NewsView.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div v-for="user in users">{{ user.title }}</div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data() {
+    return {
+      users: [],
+    };
+  },
+  created() {
+    var vm = this;
+    axios
+      .get("https://api.hnpwa.com/v0/news/1.json")
+      .then((response) => (vm.users = response.data))
+      .catch((error) => console.log(error));
+  },
+};
+</script>
+
+<style></style>
