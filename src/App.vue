cc0be1f46aba488cda40eb0fdc2aac37bd0e1765
yjh4218 <84125522+yjh4218@users.noreply.github.com>
2022-05-05 20:36:58 +0900

    router transition 설정

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
     <tool-bar></tool-bar>
-    <router-view></router-view>
+    <transition name="page">
+      <router-view></router-view>
+    </transition>
   </div>
 </template>
 
@@ -18,5 +20,15 @@ export default {
 body {
   padding: 0;
   margin: 0;
+}
+/* router transition */
+.page-enter-active,
+.page-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.page-enter-from,
+.page-leave-to {
+  opacity: 0;
 }
 </style>
