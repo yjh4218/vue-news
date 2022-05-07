be5fed5024563ae75122bca611b68b8d8f374e87
yjh4218 <84125522+yjh4218@users.noreply.github.com>
2022-05-08 00:56:43 +0900

    news css

--- src/App.vue
+++ src/App.vue
@@ -21,6 +21,20 @@ body {
   padding: 0;
   margin: 0;
 }
+a {
+  color: #34495e;
+  /* 글자 밑줄 삭제 */
+  text-decoration: none;
+}
+/* 글자 마우스 커서 올라갈 때 글자 색 변경 */
+a:hover {
+  color: #42b338;
+  text-decoration: underline;
+}
+a.router-link-exact-active {
+  /* 글자 밑줄 추가 */
+  text-decoration: underline;
+}
 /* router transition */
 .page-enter-active,
 .page-leave-active {
--- src/views/NewsView.vue
+++ src/views/NewsView.vue
@@ -1,17 +1,44 @@
 <template>
   <div>
+    <!-- ● 리팩토링 적용 후 코드 -->
+    <ul class="news-list">
+      <li v-for="news in this.$store.state.news" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ news.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="news-title">
+            <a v-bind:href="news.url"> {{ news.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ news.time_ago }} by
+            <router-link v-bind:to="`/user/${news.user}`" class="link-text">
+              {{ news.user }}
+            </router-link>
+          </small>
+        </div>
+        <!-- 
+        <small>
+          
+        </small> -->
+      </li>
+    </ul>
+
+    <!-- ● 리팩토링 적용 전 코드 -->
     <!-- <div v-for="news in this.$store.state.news">{{ news.title }}</div> -->
-    <p v-for="news in fetchNews">
+    <!-- <p v-for="news in fetchNews">
       <a v-bind:href="news.url"> {{ news.title }}</a>
       <small>
         {{ news.time_ago }} by
-        <!-- 동적 라우팅 -->
-        <!-- <router-link v-bind:to="'/user/' + news.user">{{ news.user }}</router-link> -->
         <router-link v-bind:to="`/user/${news.user}`">{{
           news.user
         }}</router-link>
       </small>
-    </p>
+    </p> -->
+    <!-- 동적 라우팅 -->
+    <!-- <router-link v-bind:to="'/user/' + news.user">{{ news.user }}</router-link> -->
   </div>
 </template>
 
@@ -29,4 +56,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b338;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
