3d68058ed4a549c44a0e47d5023df6982e6db0de
yjh4218 <84125522+yjh4218@users.noreply.github.com>
2022-05-08 22:12:16 +0900

    ask, job css

--- src/views/AskView.vue
+++ src/views/AskView.vue
@@ -1,15 +1,41 @@
 <template>
   <div>
+    <ul class="news-list">
+      <li v-for="ask in fetchAsk" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ ask.points }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="ask-title">
+            <router-link v-bind:to="`/item/${ask.id}`">
+              {{ ask.title }}
+            </router-link>
+          </p>
+          <small class="link-text">
+            {{ ask.time_ago }} by
+            <router-link v-bind:to="`/user/${ask.user}`" class="link-text">
+              {{ ask.user }}
+            </router-link>
+          </small>
+        </div>
+        <!-- 
+        <small>
+          
+        </small> -->
+      </li>
+    </ul>
     <!-- <div v-for="ask in askList">{{ ask.title }}</div> -->
     <!-- <div v-for="ask in this.$store.state.asks">{{ ask.title }}</div> -->
-    <div v-for="ask in fetchAsk">
+    <!-- <div v-for="ask in fetchAsk">
       <router-link v-bind:to="`/item/${ask.id}`"> {{ ask.title }}</router-link>
       <small>
-        {{ ask.time_ago }}
-        <!-- 동적 라우팅 -->
-        <!-- <router-link v-bind:to="'/user/' + news.user">{{ news.user }}</router-link> -->
-      </small>
-    </div>
+        {{ ask.time_ago }} -->
+    <!-- 동적 라우팅 -->
+    <!-- <router-link v-bind:to="'/user/' + news.user">{{ news.user }}</router-link> -->
+    <!-- </small>
+    </div> -->
   </div>
 </template>
 
@@ -41,4 +67,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.ask-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b338;
+}
+.ask-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
--- src/views/JobsView.vue
+++ src/views/JobsView.vue
@@ -1,11 +1,37 @@
 <template>
   <div>
+    <ul class="jobs-list">
+      <li v-for="job in fetchJobs" class="post">
+        <!-- 포인트 영역 -->
+        <div class="points">
+          {{ job.points || 0 }}
+        </div>
+        <!-- 기타 정보 영역 -->
+        <div>
+          <p class="job-title">
+            <a v-bind:href="job.url"> {{ job.title }}</a>
+          </p>
+          <small class="link-text">
+            {{ job.time_ago }} by
+            <!-- <router-link v-bind:to="`/user/${job.user}`" class="link-text">
+             -->
+            <!-- {{ job.domain }} -->
+            <!-- </router-link> -->
+            <a :href="job.url">{{ job.domain }}</a>
+          </small>
+        </div>
+        <!-- 
+        <small>
+          
+        </small> -->
+      </li>
+    </ul>
     <!-- <div v-for="job in jobList">{{ job.title }}</div> -->
     <!-- <div v-for="job in this.$store.state.jobs">{{ job.title }}</div> -->
-    <p v-for="job in fetchJobs">
+    <!-- <p v-for="job in fetchJobs">
       <a v-bind:href="job.url"> {{ job.title }}</a>
       <small>{{ job.time_ago }}, {{ job.domain }}</small>
-    </p>
+    </p> -->
   </div>
 </template>
 
@@ -31,4 +57,29 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.news-list {
+  margin: 0;
+  padding: 0;
+}
+.post {
+  list-style: none;
+  display: flex;
+  align-items: center;
+  border-bottom: 1px solid #eee;
+}
+.points {
+  width: 80px;
+  height: 60px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #42b338;
+}
+.news-title {
+  margin: 0;
+}
+.link-text {
+  color: #828282;
+}
+</style>
